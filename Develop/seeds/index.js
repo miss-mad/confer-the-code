6ec5524e25e991a90489652b9b430b65ba6e531b
miss-mad <mmlh.harris@gmail.com>
2022-10-05 17:23:13 -0400

    added comments to the seeds' index.js file

--- Develop/seeds/index.js
+++ Develop/seeds/index.js
@@ -1,11 +1,14 @@
 // npm run seed runs this file to seed each table in the blogpost database
 
+// Import all 3 seeds.js files
 const seedBlogposts = require("./blogpost-seeds");
 const seedComments = require("./comment-seeds");
 const seedUsers = require("./user-seeds");
 
+// Use the sequelize connection from the connection.js file
 const sequelize = require("../config/connection");
 
+// Function expression to seed all 3 tables using async/await, then logging a message in the terminal once done
 const seedAll = async () => {
   await sequelize.sync({ force: true });
   console.log("\n----- DATABASE SYNCED -----\n");
@@ -22,4 +25,5 @@ const seedAll = async () => {
   process.exit(0);
 };
 
+// To start seeding function
 seedAll();
