a6407c1a2d11036299c59a75f97196719c18a27d
miss-mad <mmlh.harris@gmail.com>
2022-10-07 16:08:14 -0400

    direct and use api routes (just blogpost routes)

--- Develop/controllers/api/index.js
+++ Develop/controllers/api/index.js
@@ -7,19 +7,28 @@
 // need a login/logout route
 // maybe need a signup route?
 
-// blogposts:
-// gets all blogposts
-// post a blogpost
-// update a blogpost
-// delete a blogpost
-
 // users:
 // get all users
+// post a user
 // update a user
 // delete a user - don't need
 
 // comments:
-// get all comments assoc with blogpost? or user?
 // post a comment
 // update a comment
 // delete a comment
+
+// don't need:
+// get all users
+// get all comments
+
+const router = require("express").Router();
+const blogpostRoutes = require("./blogpost-routes");
+// const commentRoutes = require("./comment-routes");
+// const userRoutes = require("./user-routes");
+
+router.use("/blogposts", blogpostRoutes);
+// router.use("/comments", commentRoutes);
+// router.use("/users", userRoutes);
+
+module.exports = router;
