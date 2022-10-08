b81e01116977a789066817b9185241fe00cad498
miss-mad <mmlh.harris@gmail.com>
2022-10-07 21:00:36 -0400

    updated index file once all routes in use

--- Develop/controllers/api/index.js
+++ Develop/controllers/api/index.js
@@ -24,11 +24,11 @@
 
 const router = require("express").Router();
 const blogpostRoutes = require("./blogpost-routes");
-// const commentRoutes = require("./comment-routes");
-// const userRoutes = require("./user-routes");
+const commentRoutes = require("./comment-routes");
+const userRoutes = require("./user-routes");
 
 router.use("/blogposts", blogpostRoutes);
-// router.use("/comments", commentRoutes);
-// router.use("/users", userRoutes);
+router.use("/comments", commentRoutes);
+router.use("/users", userRoutes);
 
 module.exports = router;
