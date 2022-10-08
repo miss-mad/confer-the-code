c503b70496c83a8d5c01276d5adb4dfcc1e50474
miss-mad <mmlh.harris@gmail.com>
2022-10-07 20:59:42 -0400

    updated seed files to aid routes

--- Develop/seeds/blogpost-seeds.js
+++ Develop/seeds/blogpost-seeds.js
@@ -1,34 +1,30 @@
 const { Blogpost } = require("../models");
 
 const blogpostData = [
+  // Do not need date_created key value pair because the sequelize table automatically creates timestamps
   {
     blogpost_name: "Blog post 1",
-    blogpost_description: "Description",
-    date_created: 2022-10-5,
+    blogpost_description: "Description1",
     user_id: 1,
   },
   {
     blogpost_name: "Blog post 2",
-    blogpost_description: "Description",
-    date_created: 2022-10-5,
+    blogpost_description: "Description2",
     user_id: 2,
   },
   {
     blogpost_name: "Blog post 3",
-    blogpost_description: "Description",
-    date_created: 2022-10-5,
+    blogpost_description: "Description3",
     user_id: 3,
   },
   {
     blogpost_name: "Blog post 4",
-    blogpost_description: "Description",
-    date_created: 2022-10-5,
+    blogpost_description: "Description4",
     user_id: 4,
   },
   {
     blogpost_name: "Blog post 5",
-    blogpost_description: "Description",
-    date_created: 2022-10-5,
+    blogpost_description: "Description5",
     user_id: 5,
   },
 ];
--- Develop/seeds/comment-seeds.js
+++ Develop/seeds/comment-seeds.js
@@ -2,27 +2,27 @@ const { Comment } = require("../models");
 
 const commentData = [
   {
-    content: "Comment here",
+    content: "Comment here1",
     blogpost_id: 1,
     user_id: 1,
   },
   {
-    content: "Comment here",
+    content: "Comment here2",
     blogpost_id: 2,
     user_id: 2,
   },
   {
-    content: "Comment here",
+    content: "Comment here3",
     blogpost_id: 3,
     user_id: 3,
   },
   {
-    content: "Comment here",
+    content: "Comment here4",
     blogpost_id: 4,
     user_id: 4,
   },
   {
-    content: "Comment here",
+    content: "Comment here5",
     blogpost_id: 5,
     user_id: 5,
   },
