fbfe54b3a8b267c19a757718e6f3cc76dae4b7bd
miss-mad <mmlh.harris@gmail.com>
2022-10-11 22:57:53 -0400

    login/logout functionality

---
+++ Develop/public/js/logout.js
@@ -0,0 +1,15 @@
+const logout = async () => {
+  const response = await fetch("/api/users/logout", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+  });
+
+  // If user presses the logout button, redirect them to the homepage that shows all blogposts without any login necessary
+  if (response.ok) {
+    document.location.replace("/");
+  } else {
+    alert(response.statusText);
+  }
+};
+
+document.querySelector("#logout").addEventListener("click", logout);
--- Develop/utils/auth.js
+++ Develop/utils/auth.js
@@ -1,11 +1,11 @@
+// Helper js function that gets exported to use in homeRoutes.js
 const withAuth = (req, res, next) => {
-    // If the user is not logged in, redirect the request to the login route
-    if (!req.session.logged_in) {
-      res.redirect('/login');
-    } else {
-      next();
-    }
-  };
-  
-  module.exports = withAuth;
-  
+  // If the user is not logged in, redirect the request to the login route
+  if (!req.session.logged_in) {
+    res.redirect('/login');
+  } else {
+    next();
+  }
+};
+
+module.exports = withAuth;
