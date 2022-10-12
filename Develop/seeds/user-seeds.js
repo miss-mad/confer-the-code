a0c63708d4039147e01ae9b0a10c633c0951e58c
miss-mad <mmlh.harris@gmail.com>
2022-10-11 22:50:31 -0400

    added hook functionality from User model

--- Develop/seeds/user-seeds.js
+++ Develop/seeds/user-seeds.js
@@ -23,6 +23,10 @@ const userData = [
   },
 ];
 
-const seedUsers = () => User.bulkCreate(userData);
+const seedUsers = () => User.bulkCreate(userData, {
+  // Use the hooks defined in the User model so that the user passwords can be hashed correctly by bcrypt
+  individualHooks: true,
+  returning: true,
+});
 
 module.exports = seedUsers;
